452e32ff7fe206f6eb1b1d68f30e285e42ee4d8b
Blakely Mayhall <blakely.c.mayhall@gmail.com>
2023-02-13 22:01:22 -0600

    Added some code, really just learning

--- Rename_Files_Tool/remove_leading_garb.py
+++ Rename_Files_Tool/remove_leading_garb.py
@@ -21,9 +21,15 @@ for subdir in subdir_list:
     os.chdir(subdir)
     file_list = os.listdir(os.getcwd())
     for file in file_list:
+        # Repalces garbage with BCM; Don't want to just remove up to first "_" everytime 
         if file.find("BCM") < 0:
             os.rename(file, "BCM_"+file[(file.find("_")+1):])
 
+        # Repalces BCM+extra string with blank
         if file.find("BCM_MIT6_096IAP11_") >= 0:
             os.rename(file, file.replace("BCM_MIT6_096IAP11_",""))
+
+        # Repalces BCM string with blank
+        if file.find("BCM_") >= 0:
+            os.rename(file, file.replace("BCM_",""))
     os.chdir('../')
